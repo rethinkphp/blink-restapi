553e2b9fb7ed0dafe1ee6236e74ab233e7884423
Jin Hu <bixuehujin@gmail.com>
2021-06-11 19:07:08 +0800

    Added --api-path and --exclude-api-path to BuildDocsCommand

--- src/commands/BuildDocsCommand.php
+++ src/commands/BuildDocsCommand.php
@@ -26,15 +26,28 @@ class BuildDocsCommand extends \blink\core\console\Command
     {
         $this->addArgument('dst', InputArgument::REQUIRED, 'The path to generate into.');
         $this->addOption('ver', null, InputOption::VALUE_REQUIRED, 'The openapi version', '');
+        $this->addOption('api-path', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Speficy the api path', []);
+        $this->addOption('exclude-api-path', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Speficy the api path to exclude', []);
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $dst = $input->getArgument('dst');
         $ver = $input->getOption('ver');
+        /** @var array $apiPaths */
+        $apiPaths = $input->getOption('api-path');
+        /** @var array $excludeApiPaths */
+        $excludeApiPaths = $input->getOption('exclude-api-path');
         
         /** @var Manager $manager */
         $manager = $this->blink->get('restapi');
+        if (! empty($apiPaths)) {
+            $manager->apiPaths = $apiPaths;
+        } 
+
+        if (! empty($excludeApiPaths)) {
+            $manager->apiPaths = array_diff($manager->apiPaths, $excludeApiPaths);
+        }
 
         $manager->generateDocs($dst, $ver);
     }
